cfed3ec2682f04606e695d0dd497248f5ff4bf32
Deep Bhatt <bhattdeep38@gmail.com>
2024-01-23 12:47:01 +0530

    cors: add support for multiple ports

--- application-server/src/index.js
+++ application-server/src/index.js
@@ -4,16 +4,23 @@ require('dotenv').config()
 const express = require('express');
 const helmet = require('helmet');
 const app = express();
-const PORT = 8080;
+const PORT = process.APP_PORT || 8080;
 const { setupTables } = require('./services/bootstrap');
 const authRoutes = require('./routes/authRoutes');
 const notesRoutes = require('./routes/notesRoutes');
 const cors = require('cors');
+
+const allowedOrigins = ['http://localhost:5173', 'http://localhost:4173'];
 const corsOptions = {
-  // react FE app
-  origin: 'http://localhost:5173',
-  optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
-}
+  origin: function (origin, callback) {
+    if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+  optionsSuccessStatus: 200 // legacy browser support
+};
 
 app.use(helmet());
 app.use(cors(corsOptions));
